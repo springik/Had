647ca9eb088d26d9ab435ff58191fa87e10f57ee
Richard Florian <risa.florian@email.cz>
2022-09-13 21:58:16 +0200

    Hadisko poprve

---
+++ Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace Had
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Red;
+            this.panel1.BackgroundImage = global::Had.Properties.Resources.th_2155205471;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Location = new System.Drawing.Point(317, 146);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(44, 38);
+            this.panel1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("OCR A Extended", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(312, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "0:00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 311);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Hadisko";
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Form1_PreviewKeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Had
+{
+    public partial class Form1 : Form
+    {
+        int s, m = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode == Keys.D)
+            {
+                if(panel1.Location.X + panel1.Width + 20 < this.Width) //kontrola kolize s hernim prostorem (horizontalni)
+                {
+                    panel1.Location = new Point(panel1.Location.X + 10, panel1.Location.Y);
+                }
+                else //restartovani pozice na druhe strane h. p.
+                {
+                    panel1.Location = new Point(0 , panel1.Location.Y);
+                }
+            }
+            else if (e.KeyCode == Keys.A)
+            {
+                if(panel1.Location.X > 0)//kontrola kolize s hernim prostorem z druhe strany (horizontalni)
+                {
+                    panel1.Location = new Point(panel1.Location.X - 10, panel1.Location.Y);
+                }
+                else //restartovani pozice na druhe strane h. p.
+                {
+                    panel1.Location = new Point(this.Width - panel1.Width - panel1.Width/3, panel1.Location.Y);
+                }
+            }
+            else if (e.KeyCode == Keys.W)
+            {
+                if(panel1.Location.Y > 0)
+                {
+                    panel1.Location = new Point(panel1.Location.X, panel1.Location.Y - 10);// kontrola kolize s hp. (vertikálně)
+                }
+                else
+                {
+                    panel1.Location = new Point(panel1.Location.X, this.Height - panel1.Height * 2);// restartování pozice z druhé strany h.p.
+                }
+            }
+            else if (e.KeyCode == Keys.S)
+            {
+                if(panel1.Location.Y + panel1.Height + 40 < this.Height)// kontrola kolize s h. p. (vert.)
+                {
+                    panel1.Location = new Point(panel1.Location.X, panel1.Location.Y + 10);
+                }
+                else
+                {
+                    panel1.Location = new Point(panel1.Location.X, 0);//restartovani pozice z druhe strany h. p.
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(s < 59)
+            {
+                s++;
+            }
+            else
+            {
+                m++;
+                s = 0;
+            }
+            if (s < 10)
+            {
+                label1.Text = m + ":0" + s;
+            }
+            else
+            {
+                label1.Text = m + ":" + s;
+            }
+        }
+    }
+}
