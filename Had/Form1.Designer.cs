8a5a22b9d91d06c2005a5a213a71d1d37794026e
Richard Florian <risa.florian@email.cz>
2022-09-19 04:27:32 +0200

    Added bubble-snake collision

--- Had/Form1.Designer.cs
+++ Had/Form1.Designer.cs
@@ -35,6 +35,8 @@ namespace Had
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.label1 = new System.Windows.Forms.Label();
             this.bubble = new System.Windows.Forms.Panel();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.tail1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // panel1
@@ -45,7 +47,7 @@ namespace Had
             this.panel1.Location = new System.Drawing.Point(317, 146);
             this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(44, 38);
+            this.panel1.Size = new System.Drawing.Size(40, 40);
             this.panel1.TabIndex = 0;
             // 
             // timer1
@@ -56,13 +58,14 @@ namespace Had
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("OCR A Extended", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(312, 20);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("OCR A Extended", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(280, 20);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.Size = new System.Drawing.Size(60, 20);
             this.label1.TabIndex = 1;
-            this.label1.Text = "0:00";
+            this.label1.Text = "00:00";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // bubble
             // 
@@ -73,16 +76,40 @@ namespace Had
             this.bubble.Size = new System.Drawing.Size(20, 20);
             this.bubble.TabIndex = 2;
             // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Font = new System.Drawing.Font("OCR A Extended", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.scoreLabel.Location = new System.Drawing.Point(403, 25);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(28, 17);
+            this.scoreLabel.TabIndex = 3;
+            this.scoreLabel.Text = "10";
+            // 
+            // tail1
+            // 
+            this.tail1.BackgroundImage = global::Had.Properties.Resources.th_2155205471;
+            this.tail1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tail1.Location = new System.Drawing.Point(550, 212);
+            this.tail1.Name = "tail1";
+            this.tail1.Size = new System.Drawing.Size(40, 40);
+            this.tail1.TabIndex = 4;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(684, 311);
+            this.Controls.Add(this.tail1);
+            this.Controls.Add(this.scoreLabel);
             this.Controls.Add(this.bubble);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
+            this.Cursor = System.Windows.Forms.Cursors.No;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "Hadisko";
             this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Form1_PreviewKeyDown);
@@ -97,6 +124,8 @@ namespace Had
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel bubble;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Panel tail1;
     }
 }
 
--- Had/Form1.cs
+++ Had/Form1.cs
@@ -12,16 +12,37 @@ namespace Had
 {
     public partial class Form1 : Form
     {
+        int score;
         int s, m = 0;
+        int scale = 20;
         Random rnd = new Random();
+        List<Panel> tail = new List<Panel>();
         public Form1()
         {
             InitializeComponent();
+            this.Height = 600;
+            this.Width = 600;
+            scale = panel1.Width;
+            label1.Location = new Point(this.Width / 2 - label1.Width / 2, 20);
+            scoreLabel.Location = new Point(this.Width/2 - scoreLabel.Width / 2 + 5, 40);
+            scoreLabel.Text = "0";
             bubble.Location = new Point(rnd.Next(0, this.Width), rnd.Next(0, this.Height));
+            //panel1.Location = new Point(this.Width / 2 - panel1.Width - panel1.Width / 3, this.Height / 2 - panel1.Height - panel1.Height / 3);
+            panel1.Location = new Point(0,0);
+            //tail.Add(panel1);
+            //tail.Add(panel1);
         }
         void moveBubble(Panel bubble) //posune bublinu na nahodnou pozici
         {
-            bubble.Location = new Point(rnd.Next(0, this.Width), rnd.Next(0, this.Height));
+            bubble.Location = new Point(rnd.Next(0, 550), rnd.Next(0, 550)); //TODO: neposouva se po gridu
+        }
+        void updateTail()
+        {
+            tail1.Location = tail[tail.Count - 2].Location;
+            //foreach (var item in tail)
+            //{
+            //    tail{x}.Location = item.Location;
+            //}
         }
 
         private void Form1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
@@ -30,54 +51,87 @@ namespace Had
             {
                 if(panel1.Location.X + panel1.Width + 20 < this.Width) //kontrola kolize s hernim prostorem (horizontalni)
                 {
-                    panel1.Location = new Point(panel1.Location.X + 10, panel1.Location.Y);
+                    panel1.Location = new Point(panel1.Location.X + scale, panel1.Location.Y);
+                    tail.Add(panel1);
+                    updateTail();
                 }
                 else //restartovani pozice na druhe strane h. p.
                 {
                     panel1.Location = new Point(0 , panel1.Location.Y);
+                    updateTail();
                 }
-                //if ()
-                //{
-                //    moveBubble(bubble);
-                //}
+                if (panel1.Bounds.IntersectsWith(bubble.Bounds))//kolize hada a bublinky
+                {
+                    moveBubble(bubble);
+                    score++;
+                    updateTail();
+                }
             }
             else if (e.KeyCode == Keys.A)
             {
                 if(panel1.Location.X > 0)//kontrola kolize s hernim prostorem z druhe strany (horizontalni)
                 {
-                    panel1.Location = new Point(panel1.Location.X - 10, panel1.Location.Y);
+                    panel1.Location = new Point(panel1.Location.X - scale, panel1.Location.Y);
+                    tail.Add(panel1);
+                    updateTail();
                 }
                 else //restartovani pozice na druhe strane h. p.
                 {
                     panel1.Location = new Point(this.Width - panel1.Width - panel1.Width/3, panel1.Location.Y);
+                    updateTail();
+                }
+                if (panel1.Bounds.IntersectsWith(bubble.Bounds))//kolize hada a bublinky
+                {
+                    moveBubble(bubble);
+                    score++;
+                    updateTail();
                 }
             }
             else if (e.KeyCode == Keys.W)
             {
                 if(panel1.Location.Y > 0)
                 {
-                    panel1.Location = new Point(panel1.Location.X, panel1.Location.Y - 10);// kontrola kolize s hp. (vertikálně)
+                    panel1.Location = new Point(panel1.Location.X, panel1.Location.Y - scale);// kontrola kolize s hp. (vertikálně)
+                    tail.Add(panel1);
+                    updateTail();
                 }
                 else
                 {
                     panel1.Location = new Point(panel1.Location.X, this.Height - panel1.Height * 2);// restartování pozice z druhé strany h.p.
+                    updateTail();
+                }
+                if (panel1.Bounds.IntersectsWith(bubble.Bounds))//kolize hada a bublinky
+                {
+                    moveBubble(bubble);
+                    score++;
+                    updateTail();
                 }
             }
             else if (e.KeyCode == Keys.S)
             {
                 if(panel1.Location.Y + panel1.Height + 40 < this.Height)// kontrola kolize s h. p. (vert.)
                 {
-                    panel1.Location = new Point(panel1.Location.X, panel1.Location.Y + 10);
+                    panel1.Location = new Point(panel1.Location.X, panel1.Location.Y + scale);
+                    tail.Add(panel1);
+                    updateTail();
                 }
                 else
                 {
                     panel1.Location = new Point(panel1.Location.X, 0);//restartovani pozice z druhe strany h. p.
+                    updateTail();
+                }
+                if (panel1.Bounds.IntersectsWith(bubble.Bounds))//kolize hada a bublinky
+                {
+                    moveBubble(bubble);
+                    score++;
+                    updateTail();
                 }
             }
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            scoreLabel.Text = Convert.ToString(score);
             if(s < 59)
             {
                 s++;
@@ -89,11 +143,26 @@ namespace Had
             }
             if (s < 10)
             {
-                label1.Text = m + ":0" + s;
+                if (m < 10)
+                {
+                    label1.Text = "0" + m + ":0" + s;
+                }
+                else
+                {
+                    label1.Text = m + ":0" + s;
+                }
             }
             else
             {
-                label1.Text = m + ":" + s;
+                if(m < 10)
+                {
+                    label1.Text = "0" + m + ":" + s;
+                }
+                else
+                {
+                    label1.Text = m + ":" + s;
+                }
+                
             }
         }
     }
